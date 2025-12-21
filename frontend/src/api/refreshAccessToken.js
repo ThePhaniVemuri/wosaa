f86d5a716e15368b42a5b242c2d0884aee73b880
phaniai <toptensteff@gmail.com>
2025-12-21 22:28:10 +0530

    apibase small change

--- frontend/src/api/refreshAccessToken.js
+++ frontend/src/api/refreshAccessToken.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:3000/api/v1";
+import { API_BASE } from "./config";
 
 export async function fetchWithRefresh(url, options = {}) {
   options = { ...options, credentials: "include" }; // include cookies now no matter what
